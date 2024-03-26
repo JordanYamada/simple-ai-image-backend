433194a5e26e17ff26ee909efd7149318a752a28
Jordan Yamada <sadaotomokane@gmail.com>
2024-03-26 23:17:18 +0000

    changed from dall-e 2 to dall-e 3 for the image description from the prompt

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -10,7 +10,7 @@ const aiImage = async (req, res) => {
   try {
     const description = req.body.description;
     const response = await openai.images.generate({
-      model: "dall-e-2",
+      model: "dall-e-3",
       prompt: description,
       n: 1,
       size: "1024x1024",
